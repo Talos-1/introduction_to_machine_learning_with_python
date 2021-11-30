828c2fb53cc0c0937bbe772479564f0d222ec7fa
Talos-1 <adityaprayoga@hotmail.com>
2021-12-01 09:59:44 +1100

    chapter 1 and part of 2 added to initial commit.

---
+++ version_script.py
@@ -0,0 +1,15 @@
+import sys
+import pandas as pd
+import matplotlib
+import numpy as np
+import scipy as sp
+import IPython
+import sklearn
+
+print("python version", sys.version)
+print("pandas version:", pd.__version__)
+print("matplotlib version:", matplotlib.__version__)
+print("numpy version:", np.__version__)
+print("scipy version:", sp.__version__)
+print("IPython version:", IPython.__version__)
+print("sci-kit learn version", sklearn.__version__)
